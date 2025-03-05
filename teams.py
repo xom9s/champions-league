bd832b3adff7bbffbf273625754526fac0d9833c
xom9s <xom9k321@gmail.com>
2025-03-05 11:43:46 +0200

    cl teams simple request, and changed some names

--- teams.py
+++ teams.py
@@ -6,9 +6,8 @@ with open('config.json', 'r') as config_file:
 
 FOOTBALL_DATA = config['FOOTBALL_DATA']
 
-uri = 'https://api.football-data.org/v4/competitions/CL/matches'
+uri = 'https://api.football-data.org//v4/competitions/CL/teams'
 headers = { 'X-Auth-Token': FOOTBALL_DATA }
-
-matches = requests.get(uri, headers=headers)
-for match in matches.json()['matches']:
-    print(match)
+teams = requests.get(uri, headers=headers)
+for team in teams.json()['teams']:
+    print(team['shortName'])
