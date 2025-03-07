a75e2a52623f0b971e3f8c4fca3737bcbc7c348b
xom9s <xom9k321@gmail.com>
2025-03-07 11:22:51 +0200

    improved structure for standings and teams

--- standings.py
+++ standings.py
@@ -1,15 +1,18 @@
 import requests
 import json
 
-with open('config.json', 'r') as config_file:
-    config = json.load(config_file)
+def get_standings():
+    with open('config.json', 'r') as config_file:
+        config = json.load(config_file)
 
-FOOTBALL_DATA = config['FOOTBALL_DATA']
+    FOOTBALL_DATA = config['FOOTBALL_DATA']
 
-uri = 'https://api.football-data.org/v4/competitions/SA/standings'
-headers = { 'X-Auth-Token': FOOTBALL_DATA}
+    uri = 'https://api.football-data.org/v4/competitions/SA/standings'
+    headers = { 'X-Auth-Token': FOOTBALL_DATA}
 
-standings = requests.get(uri, headers=headers)
-for standing in standings.json()['standings']:
-    print(standing['table'])
+    standings = requests.get(uri, headers=headers)
+    for standing in standings.json()['standings']:
+        print(standing['table'])
 
+if __name__ == '__main__':
+    get_standings()
--- teams.py
+++ teams.py
@@ -1,13 +1,17 @@
 import requests
 import json
 
-with open('config.json', 'r') as config_file:
-    config = json.load(config_file)
+def get_teams():
+    with open('config.json', 'r') as config_file:
+        config = json.load(config_file)
 
-FOOTBALL_DATA = config['FOOTBALL_DATA']
+    FOOTBALL_DATA = config['FOOTBALL_DATA']
 
-uri = 'https://api.football-data.org/v4/competitions/SA/teams'
-headers = { 'X-Auth-Token': FOOTBALL_DATA }
-teams = requests.get(uri, headers=headers)
-for team in teams.json()['teams']:
-    print(team['shortName'])
+    uri = 'https://api.football-data.org/v4/competitions/SA/teams'
+    headers = { 'X-Auth-Token': FOOTBALL_DATA }
+    teams = requests.get(uri, headers=headers)
+    for team in teams.json()['teams']:
+        print(team['shortName'])
+
+if __name__ == '__main__':
+    get_teams()
