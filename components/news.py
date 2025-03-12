b2dba623bd883779dda77fb208c7c743b452b788
xom9s <xom9k321@gmail.com>
2025-03-12 21:08:47 +0200

    updated news fetch

--- components/news.py
+++ components/news.py
@@ -1,11 +1,18 @@
 from . import requests, json, NEWS_API_KEY
 
-try:
-    uri = f'https://newsapi.org/v2/everything?q=PremierLeague&from=2025-02-12&language=en&sortBy=popularity&apiKey={NEWS_API_KEY}'
-    news = requests.get(uri)
-    news.raise_for_status()
-    for article in news.json()['articles']:
-        print(f"Title:{article['title']}, URL:{article['url']}")
-        
-except requests.exceptions.RequestException as e:
-    print (f"An error occured {e}")
+def get_news():
+    news_urls = []
+    try:
+
+        uri = f'https://newsapi.org/v2/everything?q=Serie-a&from=2025-02-12&language=en&sortBy=popularity&apiKey={NEWS_API_KEY}'
+        news = requests.get(uri)
+        news.raise_for_status()
+        for article in news.json()['articles']:
+            news_urls.append(article['url'])
+        return news_urls
+            
+    except requests.exceptions.RequestException as e:
+        print (f"An error occured {e}")
+
+if __name__ == "__main__":
+    get_news()
