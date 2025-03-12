3333a6727943a0d8e6c7e156ca544a49c8d51332
xom9s <xom9k321@gmail.com>
2025-03-13 01:33:09 +0200

    initialization file updated

--- components/__init__.py
+++ components/__init__.py
@@ -4,6 +4,15 @@ import psycopg2
 
 with open('config.json', 'r') as config_file:
         config = json.load(config_file)
-
+        
+STOCKS = config['STOCKS']
 FOOTBALL_DATA = config['FOOTBALL_DATA']
 NEWS_API_KEY = config['NEWS']
+DB_CONFIG = config['DB_CONFIG']
+
+def get_connection():
+        try:
+                conn = psycopg2.connect(**DB_CONFIG)
+                return conn
+        except psycopg2.Error as e:
+                print(f"Error has occured {e}")
