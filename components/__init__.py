283d6b1783d1d293d3184c7507207d17d169a659
xom9s <xom9k321@gmail.com>
2025-03-12 20:21:24 +0200

    updated initialization

--- components/__init__.py
+++ components/__init__.py
@@ -1,7 +1,9 @@
 import requests
 import json
+import psycopg2
 
 with open('config.json', 'r') as config_file:
         config = json.load(config_file)
 
 FOOTBALL_DATA = config['FOOTBALL_DATA']
+NEWS_API_KEY = config['NEWS']
