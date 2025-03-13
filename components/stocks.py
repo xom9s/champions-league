60b321b96a190fa612f8d47e99455332a73203bd
xom9s <xom9k321@gmail.com>
2025-03-13 13:46:58 +0200

    stocks display

--- components/stocks.py
+++ components/stocks.py
@@ -1,49 +1,26 @@
-from . import requests, get_connection, STOCKS
+import streamlit as st
+import pandas as pd
+from etl.stocks_etl import get_stocks
 
 
-def get_stocks():
-    
-    uri = f'https://financialmodelingprep.com/api/v3/stock/full/real-time-price/INTERUSD?apikey={STOCKS}'
-
-    stocks_info = requests.get(uri)
-
-    if stocks_info.status_code == 200:
-        return stocks_info.json()
-
-
-    else:
-         print(f"Error: {stocks_info.status_code}")
-
-def load_stocks_to_db():
+def display_stocks():
     data = get_stocks()
-    conn = get_connection()
 
-    if not conn:
-        return
-    cur = conn.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS stocks ( id SERIAL PRIMARY KEY,
-        symbol VARCHAR(10),
-        last_price FLOAT,
-        volume INT,
-        last_updated BIGINT);""")
     if data:
-        stock = data[0]
-        symbol = stock["symbol"]
-        last_price = stock["lastSalePrice"]
-        volume = stock["volume"]
-        last_updated = stock["lastUpdated"]
-    
-        
-
-        cur.execute("""INSERT INTO stocks (symbol, last_price, volume, last_updated) VALUES (%s, %s, %s, %s);""", (symbol, last_price, volume, last_updated))
-
-
-    
-        conn.commit()
-        cur.close()
-        conn.close()
-        print("Stock data successfully loaded")
+        stocks_data = []
+        for stock in data:
+            stocks_data.append({
+                'Symbol': stock['symbol'],
+                'Last Price': stock['lastSalePrice'],
+                'Volume': stock['volume'],
+                'Last Updated': stock['lastUpdated']
+            })
+        df = pd.DataFrame(stocks_data)
+        st.write("### Stock data")
+        st.table(df)
+    else:
+        st.write("No stock data available.")
 
 
 if __name__ == "__main__":
-    load_stocks_to_db()
+    display_stocks()
